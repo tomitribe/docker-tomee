c9f6d36c990e674cf0851489df4e9c577a1cadf2
Rod Jenkins <rodj@jenkinsrei.com>
2025-06-26 19:39:57 -0400

    added set -ex to archive pull

--- automation.inprogress/create-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-flavors=(webprofile plus plume microprofile)
-
-for flavor in ${flavors[*]}
-do
-    ./create-server.sh $1 $2 $flavor
-done
--- automation.inprogress/create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-javas=(8-jre)
-
-for java in ${javas[*]}
-do
-    ./create-version.sh $java $1
-done
